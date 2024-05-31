c4faf820e77e5a1c1aa80145dd74a81c464eff76
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-31 17:02:25 +0300

    main view

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from .models import Category, Recipe
+
+
+def main(request):
+    recipes = Recipe.objects.order_by('?')[:10]
+    return render(request, 'main.html', {
+        'recipes': recipes
+    })
 
