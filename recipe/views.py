2037ba0f18bf0df98e57200ede53b4d0221b04be
Halaiko Vitalii <galaykovitalii@knu.ua>
2024-05-31 17:26:42 +0300

    add comments

--- recipe/views.py
+++ recipe/views.py
@@ -3,15 +3,23 @@ from .models import Category, Recipe
 
 
 def main(request):
+    """
+    Renders the main page with 10 randon recipes
+    """
     recipes = Recipe.objects.order_by('?')[:10]
+
     return render(request, 'main.html', {
         'recipes': recipes
     })
 
 
 def category_detail(request, category_id):
+    """
+    Renders the category detail page with all recipes associated with that category by some category id
+    """
     category = Category.objects.get(id=category_id)
-    recipes = Recipe.objects.filter(category=category)
+    recipes_in_category = category.categories.all()
+
     return render(request, 'category_detail.html', {
-        'category': recipes
+        'category': recipes_in_category
     })
